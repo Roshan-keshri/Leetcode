d91375bdb1a7f5319dfc065ffba3e3fc3b4d4da5
Roshan Keshri <keshriroshan44@gmail.com>
2026-08-18 20:38:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (29.29%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        for (int i=0;i<m;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return findpath(m-1, n-1, dp);
+    }
+    public int findpath(int m, int n, int dp[][]){
+        if(m==0 && n==0){
+            return 1;
+        }
+        if(m<0 || n<0){
+            return 0;
+        }
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+        int up=findpath(m-1,n,dp);
+        int left=findpath(m,n-1,dp);
+        return dp[m][n]=up+left;
+    }
+}
