cb2861ed0e80cfa224baf4a21ce3d691e2aba198
Roshan Keshri <keshriroshan44@gmail.com>
2026-08-30 23:17:04 +0530

    Time: 20 ms (78.57%) | Memory: 53.8 MB (69.25%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,21 +2,16 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int idx1=text1.length();
         int idx2=text2.length();
-        int dp[][]=new int [idx1][idx2];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        int dp[][]=new int [idx1+1][idx2+1];
+        for(int i=1;i<=idx1;i++){
+            for(int j=1;j<=idx2;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
-        return findlen(text1, text2, idx1-1, idx2-1, dp);
-    }
-    public int findlen(String text1, String text2, int idx1, int idx2, int dp[][]){
-        if(idx1<0  || idx2 < 0){
-            return 0;
-        }
-        if(dp[idx1][idx2]!=-1)return dp[idx1][idx2];
-        if(text1.charAt(idx1)==text2.charAt(idx2)){
-            return dp[idx1][idx2]=1+findlen(text1, text2, idx1-1, idx2-1, dp);
-        }
-       return dp[idx1][idx2]=Math.max(findlen(text1, text2, idx1-1,idx2,dp), findlen(text1, text2, idx1, idx2-1, dp));
-
+        return dp[idx1][idx2];
     }
 }
